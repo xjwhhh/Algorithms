724ec4825de40ac01edccc210db1607ed83dd4cc
151250171 <151250171@smail.nju.edu.cn>
2017-12-22 19:13:37 +0800

    sort exercise

---
+++ src/cn/edu/nju/software151250171/sort/Frequency.java
@@ -0,0 +1,39 @@
+package cn.edu.nju.software151250171.sort;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+/**
+ * 从标准输入读取一列字符串并按照字符串出现频率由高到低的顺序打印出每个字符串及其出现次数
+ */
+public class Frequency {
+
+    public static void main(String[] args) {
+
+        // read in and sort the input strings
+        String[] a = StdIn.readAllStrings();
+        int n = a.length;
+        Arrays.sort(a);
+//
+//        // create an array of distinct strings and their frequencies
+//        Record[] records = new Record[n];
+//        String word = a[0];
+//        int freq = 1;
+//        int m = 0;
+//        for (int i = 1; i < n; i++) {
+//            if (!a[i].equals(word)) {
+//                records[m++] = new Record(word, freq);
+//                word = a[i];
+//                freq = 0;
+//            }
+//            freq++;
+//        }
+//        records[m++] = new Record(word, freq);
+//
+//        // sort by frequency and print results
+//        Arrays.sort(records, 0, m);
+//        for (int i = m-1; i >= 0; i--)
+//            StdOut.println(records[i]);
+    }
+}
---
+++ src/cn/edu/nju/software151250171/sort/SPT.java
@@ -0,0 +1,26 @@
+package cn.edu.nju.software151250171.sort;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class SPT {
+
+//    public static void main(String[] args) {
+//        int n = StdIn.readInt();
+//        Job[] jobs = new Job[n];
+//        for (int i = 0; i < n; i++) {
+//            String name = StdIn.readString();
+//            double time = StdIn.readDouble();
+//            jobs[i] = new Job(name, time);
+//        }
+//
+//        // sort jobs in ascending order of processing time
+//        Arrays.sort(jobs);
+//
+//        // print out schedule
+//        for (int i = 0; i < n; i++)
+//            StdOut.println(jobs[i]);
+//    }
+
+}
